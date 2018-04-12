88ed7144bc6985930e8cec8fd26d0a91fd124d55
Diane Mansuy <t-dmansuy@egencia.com>
2018-04-12 09:37:20 +0200

    files3

---
+++ part3/td03.js
@@ -0,0 +1,29 @@
+/* TD03 
+
+Le but de cet exercice est de créer fonction retournant une Promise 
+qui après trois secondes retournera le nombre 42 dans son 'then'.
+
+*/
+
+/* TD Part */
+
+function waitThreeSecondsAndSend42InTheThen() {
+}
+
+/* Testing part */
+let startTime = Number(new Date());
+
+function callback(returnedValue) {
+    let timeShift = parseInt((Number(new Date()) - startTime) / 1000);
+
+    if (timeShift === 3 &&
+        returnedValue === 42) {
+        console.log('TD03 :: Passed')
+    } else {
+        console.log('TD03 :: Failed')
+    }
+}
+
+waitThreeSecondsAndSend42InTheThen()
+    .then(callback)
+    .catch(error => console.error(error));
